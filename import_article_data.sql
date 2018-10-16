3116177a0d537647e5354721d41bc6dc02408de1
Martin O'Reilly <developer@mtspace.net>
2018-10-16 07:59:54 +0100

    Fix error in article import SQL by specifying column order on INSERT

--- import_article_data.sql
+++ import_article_data.sql
@@ -1,15 +1,15 @@
 Declare @JSON varchar(max)
 
 SELECT @JSON = BulkColumn
-FROM OPENROWSET (BULK 'conservativehq.com_extracted.txt', 
+FROM OPENROWSET (BULK 'youngcons.com_extracted.txt', 
     DATA_SOURCE = 'misinformation_crawl_container',
     SINGLE_CLOB) as j
 
-INSERT INTO [articles]
-SELECT site_name, article_url, crawl_date, content, microformat_metadata AS metadata FROM OPENJSON (@JSON) 
+INSERT INTO [articles](site_name, crawl_date, article_url, content, metadata)
+SELECT site_name, crawl_date, article_url, content, microformat_metadata AS metadata FROM OPENJSON (@JSON) 
 WITH(
     site_name NVARCHAR(800),
-    article_url NVARCHAR(800),
     crawl_date DATETIMEOFFSET(7),
+    article_url NVARCHAR(800),
     content NVARCHAR(max) AS JSON,
     microformat_metadata NVARCHAR(max) AS JSON) as articles
