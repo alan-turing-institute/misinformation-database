ec6b4f13557b3027ce4da230d7681ebc79e92274
Martin O'Reilly <developer@mtspace.net>
2018-10-22 22:17:08 +0100

    Update article table structure

--- create_articles_table.sql
+++ create_articles_table.sql
@@ -2,14 +2,19 @@ IF OBJECT_ID('[articles]', 'U') IS NULL
 CREATE TABLE [articles]
 (
     [id] INT NOT NULL IDENTITY PRIMARY KEY,
-    [site_name] NVARCHAR(800) NOT NULL, 
+    [crawl_id] UNIQUEIDENTIFIER,
     [crawl_date] DATETIMEOFFSET(7), -- Datetime with timezone information
-    [article_url] NVARCHAR(800) NOT NULL, -- Max non-clustered index is 1700 chars. At worst case 2 bytes per UTF-16 char, this is 850 chars. Leave spare bytes for composite indexes. Note RFC 7230, section 3.1.1 recommends supporting at least 8000 octets for URLs 
-    [content] NVARCHAR(max) NULL,
+    [site_name] NVARCHAR(800) NOT NULL, 
+    [article_url] NVARCHAR(800) NOT NULL UNIQUE, -- Max non-clustered index is 1700 chars. At worst case 2 bytes per UTF-16 char, this is 850 chars. Leave spare bytes for composite indexes. Note RFC 7230, section 3.1.1 recommends supporting at least 8000 octets for URLs 
+    [title] NVARCHAR(max) NULL,
+    [author] NVARCHAR(max) NULL,
+    [publication_date] DATETIME2(7),
+    [plain_content] NVARCHAR(max),
+    [structured_content] NVARCHAR(max) NULL,
     [metadata] NVARCHAR(max) NULL,
 );
 GO
 IF OBJECT_ID('[articles]', 'U') IS NULL
 ALTER TABLE [articles]
-ADD CONSTRAINT [Article metadata should be formatted as JSON]
+ADD CONSTRAINT [metadata_as_json]
 CHECK (ISJSON([metadata])=1);
--- import_article_data.sql
+++ import_article_data.sql
@@ -5,11 +5,20 @@ FROM OPENROWSET (BULK 'youngcons.com_extracted.txt',
     DATA_SOURCE = 'misinformation_crawl_container',
     SINGLE_CLOB) as j
 
-INSERT INTO [articles](site_name, crawl_date, article_url, content, metadata)
-SELECT site_name, crawl_date, article_url, content, microformat_metadata AS metadata FROM OPENJSON (@JSON) 
+INSERT INTO [articles](crawl_id, crawl_date, site_name, article_url,
+    title, author, publication_date, plain_content,
+    structured_content, metadata)
+SELECT 
+    crawl_id, crawl_date, site_name, article_url, title, author, publication_date,
+    plain_content, structured_content, metadata FROM OPENJSON (@JSON) 
 WITH(
-    site_name NVARCHAR(800),
+    crawl_id UNIQUEIDENTIFIER,
     crawl_date DATETIMEOFFSET(7),
+    site_name NVARCHAR(800),
     article_url NVARCHAR(800),
-    content NVARCHAR(max) AS JSON,
-    microformat_metadata NVARCHAR(max) AS JSON) as articles
+    title NVARCHAR(max),
+    author NVARCHAR(max),
+    publication_date DATETIME2(7),
+    plain_content NVARCHAR(max) AS JSON,
+    structured_content NVARCHAR(max) AS JSON,
+    metadata NVARCHAR(max) AS JSON) as articles
