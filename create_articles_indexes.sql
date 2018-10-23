3b45d5f2c7b7c85467903f80d0fa632c7d05a0b5
Martin O'Reilly <developer@mtspace.net>
2018-10-23 11:32:27 +0100

    Amend article index SQL to succeed when indexes don't exist
    - Also temporarily changes article table name for data patch

--- create_articles_indexes.sql
+++ create_articles_indexes.sql
@@ -1,5 +1,11 @@
--- Recreate indexes
-CREATE UNIQUE INDEX [article_url_index] ON [articles] ([article_url] ASC) WITH (DROP_EXISTING = ON);
-CREATE INDEX [site_name_index] ON [articles] ([site_name] ASC) WITH (DROP_EXISTING = ON);
-CREATE INDEX [crawl_run_index] ON [articles] ([site_name] ASC, [crawl_date] ASC) WITH (DROP_EXISTING = ON);
+IF EXISTS(SELECT * FROM sys.indexes WHERE object_id = object_id('articles_v2') AND NAME ='article_url_index')
+    DROP INDEX article_url_index ON articles_v2;
+IF EXISTS(SELECT * FROM sys.indexes WHERE object_id = object_id('articles_v2') AND NAME ='site_name_index')
+    DROP INDEX site_name_index ON articles_v2;
+IF EXISTS(SELECT * FROM sys.indexes WHERE object_id = object_id('articles_v2') AND NAME ='crawl_run_index')
+    DROP INDEX crawl_run_index ON articles_v2;
+GO
+CREATE UNIQUE INDEX [article_url_index] ON [articles_v2] ([article_url] ASC);
+CREATE INDEX [site_name_index] ON [articles_v2] ([site_name] ASC);
+CREATE INDEX [crawl_run_index] ON [articles_v2] ([site_name] ASC, [crawl_date] ASC);
 GO
