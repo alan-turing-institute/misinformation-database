e02eff5a02e1fc7c8e39d81d5ee6f03af104d19f
James Robinson <james.em.robinson@gmail.com>
2019-05-21 17:38:52 +0100

    Added function to spot unfilled items

--- count_articles_per_month.sql
+++ count_articles_per_month.sql
@@ -1,4 +1,4 @@
-SELECT DATEPART(month, publication_datetime) AS month, DATEPART(year, publication_datetime) AS year, COUNT(*) AS num_articles
+SELECT site_name, CAST(publication_datetime as varchar(7)) AS date, COUNT(*) AS num_articles
 FROM articles_dev
-WHERE site_name='prisonplanet.com'
-GROUP BY DATEPART(year, publication_datetime), DATEPART(month, publication_datetime)
+GROUP BY site_name, CAST(publication_datetime as varchar(7))
+ORDER BY date
